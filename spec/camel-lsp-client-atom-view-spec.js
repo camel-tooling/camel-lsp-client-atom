20b20644e6c75c5770e81e4b7d5fe6868a64d9f3
Aurélien Pupier <apupier@redhat.com>
2019-07-25 11:18:04 +0200

    Atom package generation #1
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

---
+++ spec/camel-lsp-client-atom-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CamelLspClientAtomView from '../lib/camel-lsp-client-atom-view';
+
+describe('CamelLspClientAtomView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
