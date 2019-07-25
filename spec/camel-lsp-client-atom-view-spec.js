cfde645e3a863bb71e1e248633f1e5b618ad6cfc
Aurélien Pupier <apupier@redhat.com>
2019-07-25 11:25:56 +0200

    Remove generic stuff and use Camel naming #1
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- spec/camel-lsp-client-atom-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import CamelLspClientAtomView from '../lib/camel-lsp-client-atom-view';
-
-describe('CamelLspClientAtomView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
